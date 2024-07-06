b316a96059a516e2f86abaf01189c0ddd628fa98
FedericoScoles912 <163576246+FedericoScoles912@users.noreply.github.com>
2024-07-06 19:55:11 -0300

    Update actividad2.php

---
+++ actividad2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Array de Enteros</title>
+</head>
+<body>
+    <?php
+    $coches = array(1, 2, 3, 4, 5, 6, 7, 8);
+    echo "El elemento con índice 5 es: " . $coches[5] . "<br>";
+    ?>
+</body>
+</html>
