ba9bbbc871ca4e5675e8e7c52a9b08622cf9a7da
FedericoScoles912 <163576246+FedericoScoles912@users.noreply.github.com>
2024-07-06 19:56:45 -0300

    Update actividad3.php

---
+++ actividad3.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Array de Números Decimales</title>
+</head>
+<body>
+    <?php
+    $importe = array(10.5, 20.75, 30.25, 40.50);
+    echo "El elemento con índice 1 es: " . $importe[1] . "<br>";
+    ?>
+</body>
+</html>
