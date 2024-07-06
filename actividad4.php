ae51ebfc7a30489efb99272b18ddcfbd4a2e777c
FedericoScoles912 <163576246+FedericoScoles912@users.noreply.github.com>
2024-07-06 19:57:37 -0300

    Update actividad4.php

---
+++ actividad4.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Array de Booleanos</title>
+</head>
+<body>
+    <?php
+    $confirmado = array(true, false, true, true, false, true);
+    echo "El elemento con índice 0 es: " . ($confirmado[0] ? 'true' : 'false') . "<br>";
+    ?>
+</body>
+</html>
